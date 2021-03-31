8ac8eaac93e5bf9434c72d9c71cc34296f23032e
daleinen7 <daleinen@gmail.com>
2021-03-31 17:16:08 -0500

    through interests

--- src/components/InterestBlock/index.jsx
+++ src/components/InterestBlock/index.jsx
@@ -3,14 +3,34 @@ import styled from 'styled-components';
 
 const StyledDiv = styled.div`
   width: 250px;
-  height: 250px;
+  height: 190px;
   background: var(--gray);
+  padding: 14px;
+  margin-bottom: 12px;
+
+  .interestHeading {
+    display: flex;
+    align-items: baseline;
+  }
+
+  h3 {
+    font-size: 1rem;
+    font-weight: semi-bold;
+    margin-left: 1rem;
+  }
+  p {
+    font-size: 0.86rem;
+    margin-top: 0;
+  }
 `;
 
 export default function InterestBlock(props) {
   return(
     <StyledDiv>
-      <h3>{props.title}</h3>
+      <div className="interestHeading">
+        <img src={props.img} alt={props.img}/>
+        <h3>{props.title}</h3>
+      </div>
       <p>{props.copy}</p>
     </StyledDiv>
   )
--- src/components/OtherInterests/index.jsx
+++ src/components/OtherInterests/index.jsx
@@ -1,42 +1,58 @@
 import React from 'react';
 import InterestBlock from '../InterestBlock';
 import styled from 'styled-components';
+import audioEngineering from '../../images/audio.svg';
+import game from '../../images/game.svg';
+import instrument from '../../images/instrument.svg';
+import cooking from '../../images/cooking.svg';
+import pictureframing from '../../images/pictureframing.svg';
 
 
 const StyledDiv = styled.div`
-  h2 {
-    text-align: center;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  margin-top: 100px;
+
+  .interestsGrid {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    width: 70%;
   }
 `;
+
 export default function OtherInterests() {
   return(
     <StyledDiv>
       <h2>Other Interests</h2>
-      <InterestBlock
-        title="Audio Engineering"
-        copy="I've spent time as an audio engineer for a commercial production house in Chicago. I still do mastering work for musicians when I have the time. Check out my page explaining that coming soon."
-        img="audio"
-      />
-      <InterestBlock
-        title="Game Development"
-        copy="Many of the basic concepts of programming I learned from my time developing indie games in my free time."
-        img="game"
-      />
-      <InterestBlock
-        title="Guitars and Synthesizers"
-        copy="I play guitar and program synthesizers when no one is around."
-        img="keyboard"
-      />
-      <InterestBlock
-        title="Cooking"
-        copy="Yet another outlet that allows art, science and techique to converge into something extremely useful."
-        img="cooking"
-      />
-      <InterestBlock
-        title="Picture Framing"
-        copy="I spent 15 years in the framing industry. This prepared me to work on a team and gather a good sense of basic design principles."
-        img="pictureframing"
-      />
+      <div className="interestsGrid">
+        <InterestBlock
+          title="Audio Engineering"
+          copy="I've spent time as an audio engineer for a commercial production house in Chicago. I still do mastering work for musicians when I have the time. Check out my page explaining that coming soon."
+          img={audioEngineering}
+        />
+        <InterestBlock
+          title="Game Development"
+          copy="Many of the basic concepts of programming I learned from my time developing indie games in my free time."
+          img={game}
+        />
+        <InterestBlock
+          title="Guitars and Keys"
+          copy="I play guitar and program synthesizers when no one is around."
+          img={instrument}
+        />
+        <InterestBlock
+          title="Cooking"
+          copy="Yet another outlet that allows art, science and techique to converge into something extremely useful."
+          img={cooking}
+        />
+        <InterestBlock
+          title="Picture Framing"
+          copy="I spent 15 years in the framing industry. This prepared me to work on a team and gather a good sense of basic design principles."
+          img={pictureframing}
+        />
+      </div>
     </StyledDiv>
   )
 }
