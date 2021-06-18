c4fa02bbf9ba64e188f28fd31ff24dd8ac9acf3d
daleinen7 <daleinen@gmail.com>
2021-06-18 12:25:59 -0500

    add dumb as a mug to portfolio

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import ProjectBlock from '../ProjectBlock';
 import dotsAndBoxes from '../../images/dots-and-boxes.jpg';
-import collabratory from '../../images/collabratory.jpg';
+import daam from '../../images/daam.jpg';
 import seabound from '../../images/seabound.jpg';
 import colEcho from '../../images/colEcho.jpg';
 import adch from '../../images/adch.jpg';
@@ -34,60 +34,60 @@ export default function Projects() {
       <a href="/#" id="projects"><h2>Projects</h2></a>
       <div className="projectGrid">
         <ProjectBlock
-          title="Astro Defense Code Hero"
-          sub="A typing game to practice web development knowledge"
-          copy="Defend an orbital space station from asteroids by answering questions relating to web development technologies. A custom rudimentary React animation system moves astroids across the screen"
+          title='Astro Defense Code Hero'
+          sub='A typing game to practice web development knowledge'
+          copy='Defend an orbital space station from asteroids by answering questions relating to web development technologies. A custom rudimentary React animation system moves astroids across the screen'
           techUsed={['React', 'MongoDB']}
           img={adch}
-          direction="column"
+          direction='column'
           link={'https://astro-defense-code-hero.herokuapp.com/'}
           github={'https://github.com/daleinen7/astro-defense-code-hero'}
           key={1}
         />
-        <div className="horizontalBlock">
+        <div className='horizontalBlock'>
           <ProjectBlock
-            title="Col-Echo"
-            sub="An online open-mic platform"
-            copy="Project built with a team of 3 using Django. I lead the backend development but had a hand in the direction of the front end design. This project is a prototype and is currently being rebuilt using NextJS"
+            title='Col-Echo'
+            sub='An online open-mic platform'
+            copy='Project built with a team of 3 using Django. I lead the backend development but had a hand in the direction of the front end design. This project is a prototype and is currently being rebuilt using NextJS'
             techUsed={['Django', 'AWS']}
             img={colEcho}
-            direction="row"
+            direction='row'
             link={'https://col-echo.herokuapp.com/'}
             github={'https://github.com/daleinen7/col-echo-django'}
             key={2}
           />
           <ProjectBlock
-            title="Seabound"
-            sub="Work in progress pixel art adventure game"
-            copy="built in Construct 3. Between writing, art, audio, programming and managing a small group, this project demonstrates my versitility. This project is a work in progress."
+            title='Seabound'
+            sub='Work in progress pixel art adventure game'
+            copy='built in Construct 3. Between writing, art, audio, programming and managing a small group, this project demonstrates my versitility. This project is a work in progress.'
             techUsed={['Construct 3', 'Aseprite']}
             img={seabound}
-            direction="row"
+            direction='row'
             link={'https://pixelisfaded.itch.io/seabound'}
             github={'https://github.com/daleinen7/seabound-adventure'}
             key={3}
           />
         </div>
         <ProjectBlock
-          title="Dots and Boxes and Synthesizers"
-          sub="A simple game with a musical twist"
-          copy="Experimental Javascript game that takes the classic pass time and adds an experimental musical element."
+          title='Dots and Boxes and Synthesizers'
+          sub='A simple game with a musical twist'
+          copy='Experimental Javascript game that takes the classic pass time and adds an experimental musical element.'
           techUsed={['HTML', 'CSS', 'Javascript']}
           img={dotsAndBoxes}
-          direction="row"
+          direction='row'
           link={'https://daleinen7.github.io/dots-and-boxes-and-synthesizers/'}
           github={'https://github.com/daleinen7/dots-and-boxes-and-synthesizers'}
           key={4}
         />
         <ProjectBlock
-          title="The Collabratory"
-          sub={'Experimental User Submitted "Oblique Strategies-Like"'}
-          copy="Be fed suggestions on where to take your next musical project. Built with Node and Express JS."
-          techUsed={['Node.JS', 'Express', 'MongoDB', 'Mongoose']}
-          img={collabratory}
-          direction="row"
-          link={'https://the-collabratory.herokuapp.com/'}
-          github={'https://github.com/daleinen7/thecollabratory'}
+          title='Dumb as a Mug'
+          sub='Rebuilt PHP framework Webcomic'
+          copy='Be fed suggestions on where to take your next musical project. Built with Node and Express JS.'
+          techUsed={['Gatsby', 'DatoCMS']}
+          img={daam}
+          direction='row'
+          link={'https://dumbasamug.com/'}
+          github={'https://github.com/daleinen7/dumb-as-a-mug-gatsby'}
           key={5}
         />
       </div>
